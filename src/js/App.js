833485dc855d022575ccdb8644e3a7dd6cbf023a
Fredrik <fredrik.hobert@outlook.com>
2018-12-07 14:26:59 +0100

    Foundation

--- src/js/App.js
+++ src/js/App.js
@@ -1,26 +1,113 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import AddAdBtn from './PostAd';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-                  <Text />
+                  <Title />
+                  <AddAdBtn />
+                  <AdsList />
         </header>
       </div>
     );
   }
 }
 
-class Text extends Component {
+class Title extends Component {
   render() {
     return (
       <div className="SomeText">
-      Hello sir!
+        <h1>Ad Postings!</h1>
+        <hr></hr>
       </div>
     );
   }
+}
+
+
+
+function Pictures(props){
+
+  const pics = props.value.map((p) =>
+    <li key={p.id}>{p.url}</li>
+  );
+
+  return(
+    <div>{pics[0]}</div>
+
+  )
+}
+
+function ListItem(props){
+
+  return (
+    <li><Pictures value={props.value.pictures}/> | {props.value.title} | {props.value.text} | {props.value.category}</li>
+  );
+}
+
+
+function AdsList (){
+  const ads = GetAdsData();
+
+  const listItems = ads.map((ad) =>
+    <ListItem key={ad.id} value={ad} />
+  );
+
+  return(
+    <ul>{listItems}</ul>
+  );
+
+}
+
+function GetAdsData(){
+  return [
+    {
+        "id": 1,
+        "title": "car for sale",
+        "text": "so cool",
+        "category": "lowrider",
+        "pictures": [
+            {
+                "id": 1,
+                "adInfoId": 1,
+                "title": "testtitle",
+                "url": "testurl"
+            }
+        ]
+    },
+    {
+      "id": 2,
+        "title": "Nice cat",
+        "text": "Very sweet!",
+        "category": "animal",
+        "pictures": [
+            {
+                "id": 1,
+                "adInfoId": 2,
+                "title": "testtitle",
+                "url": "testurl"
+            }
+        ]
+    },
+    {
+      "id": 3,
+        "title": "Old computer",
+        "text": "Big and beautiful.",
+        "category": "Electronics",
+        "pictures": []
+    },
+    {
+      "id": 4,
+        "title": "Bed",
+        "text": "New fresh n cosy bed",
+        "category": "Furniture",
+        "pictures": []
+    },
+    
+]
 }
 
 export default App;
---
+++ src/js/PostAd.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddAdBtn extends Component {
+    render() {
+      return (
+        <div className="SomeText">
+          <button>+ Post ad</button>
+        </div>
+      );
+    }
+  }
+
+  export default AddAdBtn;
