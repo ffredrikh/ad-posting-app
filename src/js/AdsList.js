a2e0ab32a7f179b8d4084d94bc64797c52c03ed8
Fredrik <fredrik.hobert@outlook.com>
2018-12-09 19:49:47 +0100

    Fetch data from api

--- src/js/AdsList.js
+++ src/js/AdsList.js
@@ -8,7 +8,7 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 // Tmp testdata
-import GetAdsData from './TestData';
+//import GetAdsData from './TestData';
 
 const styles = theme => ({
     root: {
@@ -21,26 +21,73 @@ const styles = theme => ({
     },
   });
 
-  function AdsList(props) {
-    const { classes } = props;
-    let ads = GetAdsData();
+  class AdsList extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        items: [],
+        
+      };
+      
+    }
 
-    const listItems = ads.map((ad) =>
-      <Items key={ad.id} value={props} adinfo={ad} />
-    );
+    componentDidMount() {
+        fetch("https://localhost:5001/api/ads")
+          .then(res => res.json())
+          .then(
+            (result) => {
+              this.setState({
+                isLoaded: true,
+                items: result             
+              });
+            },
+       
+            // let ads = GetAdsData();
+                       
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            }
+          ); 
+      }
 
-    return (
-      <List className={classes.root}>
-        {listItems}
-      </List>
-    );
-  }
+      render() {
+        
+        const { error, isLoaded, items } = this.state;
+        const { classes } = this.props; 
+
+        const listItems = items.map((ad) =>
+        <Items key={ad.id} value={this.props} adinfo={ad} />);
+
+        console.log(this.state);
+
+        if (error) {
+          return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+          return <div>Loading...</div>;
+        } else {
+          return (
+            <List className={classes.root}>
+          {listItems}
+            </List>
+          );
+        }
+      }
+}
+
 
   function Items(props)
   {
     const { classes } = props.value;   
     return(
-        <ListItem alignItems="flex-start">
+        <ListItem key={props.adinfo.id} alignItems="flex-start">
           <ListItemAvatar>
             <Avatar alt="t" src="/img/wolf.png">          
             </Avatar>
